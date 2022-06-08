dc57b3cecf33e6119cd7a856aa6407daab112e00
GokulPrasanthT <gokulsanjay67@mail.com>
2022-06-08 10:43:29 +0530

    defultMethods

---
+++ src/com/chainsys/bestpractices/defultMethods/Ivehicle.java
@@ -0,0 +1,28 @@
+package com.chainsys.bestpractices.defultMethods;
+
+public interface Ivehicle {
+
+	void start();
+
+	default void move(){
+		 System.out.println("vehicle is moving");
+		 }
+}
+
+class Car implements Ivehicle {
+
+	@Override
+		public void start() {
+		 System.out.println("car is starting"); 
+		 }
+}
+
+class Bike implements Ivehicle {
+
+	@Override
+		 public void start() {
+		 System.out.println("bike is starting"); 
+		}
+}
+
+
---
+++ src/com/chainsys/bestpractices/defultMethods/TestVechicle.java
@@ -0,0 +1,13 @@
+package com.chainsys.bestpractices.defultMethods;
+
+public class TestVechicle {
+	public static void main(String[] args) {
+			 Car firstCar= new Car();
+			firstCar.start();
+			 firstCar.move();
+			 Bike firstBike=new Bike();
+			 firstBike.start();
+			 firstBike.move();
+			 }
+
+}
